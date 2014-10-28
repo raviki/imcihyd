f17af0cd647a9ba9c90cd4bdaf2116a1f84821d9
Istpl Dev <dev@istpl.co>
2014-10-29 00:26:40 +0530

    contact us page

--- app/controllers/contactus_controller.rb
+++ app/controllers/contactus_controller.rb
@@ -4,6 +4,7 @@ class ContactusController < ApplicationController
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
+      marker.infowindow location.address
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :locations
 
   get 'password_resets/update'
 
